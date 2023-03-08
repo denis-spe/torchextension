ecb876bf18ff03e75dcecb255c8e48902d34898f
denis-spe <denisbrian07@gmail.com>
2023-03-08 23:59:50 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+# === Glory Be To God ====
+
+from setuptools import setup
+
+setup(
+   name='torchextension',
+   version='0.0.1',
+   author='denis-spe',
+   author_email='denisbrian07@gmail.com',
+   packages=['torchextension', 'torchextension.test'],
+   url='http://pypi.python.org/pypi/torchextension/',
+   license='LICENSE.txt',
+   description='torch sequential extension',
+   long_description=open('README.txt').read(),
+   install_requires=[
+      'torchinfo',
+      'tqdm'
+   ],
+)
---
+++ torchextension/metricsInterface.py
@@ -0,0 +1,15 @@
+# === Glory Be To God ====
+
+# Import libraries
+from abc import ABC
+from dataclasses import dataclass
+from typing import Union
+from torch import Tensor
+
+
+@dataclass
+class Metric(ABC):
+    name = ...
+
+    def __call__(self, y: Tensor, yhat: Tensor) -> Union[int, float]:
+        pass
